6743a9e89d427cac2a44cc5ff3ed41c83f0c9fc9
Tailan <tailansanttos02@gmail.com>
2025-03-05 16:41:33 -0300

    feat: Criado Repositorio do estabelecimento

--- src/main/java/com/tailan/estacionamento/repository/EstabelecimentoRepository.java
+++ src/main/java/com/tailan/estacionamento/repository/EstabelecimentoRepository.java
@@ -4,6 +4,9 @@ import com.tailan.estacionamento.entities.Estabelecimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, String> {
+    List<Estabelecimento> findByNome(String nome);
 }
