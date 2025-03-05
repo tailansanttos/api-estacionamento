8ed7e64e66d1bd47dceb8cf227da9e83745881a1
Tailan <tailansanttos02@gmail.com>
2025-03-05 16:41:22 -0300

    feat: Criado VeiculoDto

--- src/main/java/com/tailan/estacionamento/dtos/VeiculoDto.java
+++ src/main/java/com/tailan/estacionamento/dtos/VeiculoDto.java
@@ -3,4 +3,6 @@ package com.tailan.estacionamento.dtos;
 import com.tailan.estacionamento.entities.VeiculoType;
 
 public record VeiculoDto(String  marca, String  modelo, String cor, String placa, VeiculoType tipo){
+
+
 }
