c37358049684872f284843e6de854d51162d52d6
Tailan <tailansanttos02@gmail.com>
2025-03-05 17:06:56 -0300

    feat: Atualização entidade veiculo

--- src/main/java/com/tailan/estacionamento/dtos/VeiculoDto.java
+++ src/main/java/com/tailan/estacionamento/dtos/VeiculoDto.java
@@ -1,8 +1,12 @@
 package com.tailan.estacionamento.dtos;
 
+import com.tailan.estacionamento.entities.Veiculo;
 import com.tailan.estacionamento.entities.VeiculoType;
 
 public record VeiculoDto(String  marca, String  modelo, String cor, String placa, VeiculoType tipo){
+    public VeiculoDto(Veiculo veiculo){
+        this(veiculo.getMarca(), veiculo.getModelo(), veiculo.getCor(), veiculo.getPlaca(), veiculo.getTipo());
+    }
 
 
 }
